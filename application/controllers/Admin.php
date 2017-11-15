9fa054d7bca55a441e09c76606abfd75d2f74a8e
rianbahari <rianbahari@users.noreply.github.com>
2017-11-15 13:35:35 +0700

    new website

---
+++ application/controllers/Admin.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		if ($this->session->userdata('logged_in')) {
+			$this->load->view('dashboard_lr');
+		}else{
+			$baseurl = base_url().'login';
+			$this->output->set_header('refresh:0.1; url='.$baseurl.'');			
+		}
+	}
+}
